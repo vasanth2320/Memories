01913137843538c06f00c79d9a49e1a5357d5d20
username <vasanthkumars2025@gmail.com>
2023-03-21 09:43:01 +0530

    uploading basic files

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+const PASSOWORD = 'hOC0SLb4fqk06Qww';
+
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = `mongodb+srv://Memories-001:${PASSOWORD}@memories.rjliq11.mongodb.net/?retryWrites=true&w=majority`;
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+mongoose.connect(CONNECTION_URL, { 
+        useNewUrlParser: true, 
+        // useUnifiedTology: true // Error: option useunifiedtology is not supported
+    })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set("useFindAndModify", false);
