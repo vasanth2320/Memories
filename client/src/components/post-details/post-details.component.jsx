f22aaf9f8a36d084963dc4c169d3c9672e96489d
username <vasanthkumars2025@gmail.com>
2023-04-08 16:42:49 +0530

    resolved error in post details page

--- client/src/components/post-details/post-details.component.jsx
+++ client/src/components/post-details/post-details.component.jsx
@@ -80,7 +80,7 @@ const PostDetails = () => {
           <img className={classes.media} src={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt={post.title} />
         </div>
       </div>
-      {recommendedPosts.length && (
+      {!!recommendedPosts.length && (
         <div className={classes.section}>
           <Typography gutterBottom variant="h5">You might also like:</Typography>
           <Divider />
